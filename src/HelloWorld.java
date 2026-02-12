52c197b12a1c7e5c781e2de2d58c02cc78d3f284
anshikamodi14 <Anshikamodi14@gmail.com>
2026-02-12 15:57:05 +0530

    integer addition

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+public static void main (String[]args) {
+    int number1=9,number2=8,sum=0;
+    sum =number1+number2;
+    System.out.println(
+            sum
+    );
+}
+}
